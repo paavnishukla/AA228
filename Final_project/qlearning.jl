16d9592c515bdd0c3db813da6501ad62e0fd1de0
Paavni <paavni.s@gmail.com>
2023-03-12 17:25:09 -0700

    Mar12_dump1

---
+++ Final_project/qlearning.jl
@@ -0,0 +1,182 @@
+using CSV
+using DataFrames
+using DelimitedFiles
+using Plots
+using LinearAlgebra
+using Dates, DayCounts
+### Q learning implementation
+mutable struct QLearning
+#S # state space (assumes 1:nstates)
+#A #action space (assumes 1:nactions)
+Y #discount
+Q #action value function
+alpha #learning rate
+end
+
+function update!(model::QLearning, s, a, r, sbar)
+    Y, Q, alpha = model.Y, model.Q, model.alpha
+    #print(keys(Q))
+    Q[(s,a)] += alpha*(r + Y*maximum([Q[(sbar,"b")],Q[(sbar,"w")]]) - Q[(s,a)])
+    return model
+end
+
+function allargmax(a)
+    m = maximum(a)
+    filter(i -> a[i] == m, eachindex(a))
+end
+function create_csv_octoparse(inputfile,refdate)
+    outputfile = replace(inputfile,".csv" => "_mdp.csv")
+    outputfile = open(outputfile,"w")
+    write(outputfile, "s,a,r,sp", "\n")     
+    for row in CSV.Rows(inputfile)
+        flightCode = row.flightCode
+        url = row.Page_URL
+        url2 = split(url,"India")
+        url3 = split(url2[3],"&")
+        url4 = split(url3[1],"C")
+        date = Date(url4[2],dateformat"d/m/y")
+        days_left = Dates.value(date-refdate)
+        print(days_left)
+        num = row.NumFlight
+        flight = string(flightCode,num)
+        #days_left = parse(Int64,row.days_left)
+        price = parse(Int64,row.Price)
+        #print("\n",flight,days_left,price) 
+        s = string(flight,'_',days_left) 
+        if(days_left > 0)
+            sp = string(flight,'_',days_left-1)
+            #sp = replace(sp,"-"=>"")
+            a = "w"
+            r = 0.0
+            mdptuple = s,a,r,sp
+            write(outputfile, join(mdptuple,","), "\n")
+
+        # elseif(days_left == 0)
+        #     sp = "notravel"
+        #     a = "w"
+        #     r = -1000000
+        #     mdptuple = s,a,r,sp
+        #     write(outputfile, join(mdptuple,","), "\n")     
+        end
+
+        sp = "travel"
+        a = "b"
+        r = Float64(100000/price)
+        #r = Float64(-price)
+        mdptuple = s,a,r,sp 
+        write(outputfile, join(mdptuple,","), "\n")
+
+    end
+    mdptuple = "travel","b",0,"travel" 
+    write(outputfile, join(mdptuple,","),"\n")
+    mdptuple = "travel","w",0,"travel"
+    write(outputfile, join(mdptuple,","),"\n")
+    mdptuple = "notravel","b",0,"notravel"
+    write(outputfile, join(mdptuple,","),"\n")
+    mdptuple = "notravel","w",0,"notravel"
+    write(outputfile, join(mdptuple,","),"\n")
+
+    close(outputfile)
+end
+function create_csv(inputfile)
+    outputfile = replace(inputfile,".csv" => "_mdp.csv")
+    outputfile = open(outputfile,"w")
+    write(outputfile, "s,a,r,sp", "\n")     
+    for row in CSV.Rows(inputfile)
+        flight = row.flight
+        days_left = parse(Int64,row.days_left)
+        price = parse(Int64,row.price)
+        #print("\n",flight,days_left,price) 
+        s = string(flight,'_',days_left) 
+        s = replace(s,"-"=>"")
+        if(days_left > 1)
+            sp = string(flight,'_',days_left-1)
+            sp = replace(sp,"-"=>"")
+            a = "w"
+            r = 0
+            mdptuple = s,a,r,sp
+            write(outputfile, join(mdptuple,","), "\n")
+
+        elseif(days_left == 1)
+            sp = "notravel"
+            a = "w"
+            r = -1000000
+            mdptuple = s,a,r,sp
+            write(outputfile, join(mdptuple,","), "\n")     
+        end
+
+        sp = "travel"
+        a = "b"
+        r = -price
+        mdptuple = s,a,r,sp 
+        write(outputfile, join(mdptuple,","), "\n")
+
+    end
+    mdptuple = "travel","b",0,"travel" 
+    write(outputfile, join(mdptuple,","),"\n")
+    mdptuple = "travel","w",0,"travel"
+    write(outputfile, join(mdptuple,","),"\n")
+    mdptuple = "notravel","b",0,"notravel"
+    write(outputfile, join(mdptuple,","),"\n")
+    mdptuple = "notravel","w",0,"notravel"
+    write(outputfile, join(mdptuple,","),"\n")
+
+    close(outputfile)
+end
+function parse_file2func(inputfile)
+    Y = 1
+    alpha = 0.1
+    #m = 1561*49 + 1
+    #n = 2
+    #S = collect(1:m)
+    #A = collect(1:n)
+    Q = Dict{Tuple{String,String},Float64}()
+    states = Set()
+    for row in CSV.Rows(inputfile)
+        s = row.s
+        a = row.a
+        sp = row.sp
+        Q[(s,"b")] = 0
+        Q[(sp,"b")] = 0
+        Q[(s,"w")] = 0
+        Q[(sp,"w")] = 0
+        push!(states,s)
+        push!(states,sp)
+
+
+    end
+    Q[("notravel","b")] = 0
+    Q[("notravel","w")] = 0
+    Q[("travel","b")] = 0
+    Q[("travel","w")] = 0
+
+    model = QLearning(Y,Q,alpha)
+    for j in 1:10
+        for row in CSV.Rows(inputfile)
+            s = row.s
+            a = row.a
+            r = parse(Float64,row.r)
+            sp = row.sp
+
+            update!(model, s,a,r,sp)
+            print("\nline",s,",",a,",",r,",",sp,",",Q[(s,a)])
+        end
+    end
+
+    #print("Q is\n",Q,"\n")
+    outputfile = replace(inputfile,"csv" => "policy")
+    outputfile = open(outputfile,"w")
+
+    for state in sort!(collect(states))
+        maxaction = (Q[(state,"w")] <  Q[(state,"b")]) ? "b" : "w"
+        write(outputfile, state,",", maxaction, "\n")     
+    end
+    print(Q)
+    close(outputfile)
+
+end
+inputfile = ARGS[1]
+refdate = Date("13/03/2023",dateformat"d/m/y")
+create_csv_octoparse(inputfile,refdate)
+inputfile = replace(inputfile,".csv" => "_mdp.csv")
+parse_file2func(inputfile)
